de3034524c661526ddb02466a83d2cd8b7526030
Sewbesew Bekele <bekeles@imsweb.com>
2019-09-20 10:54:33 -0400

    Reverted wrongly committed changes (#48)

--- src/main/java/com/imsweb/mph/MphInput.java
+++ src/main/java/com/imsweb/mph/MphInput.java
@@ -29,10 +29,6 @@ public class MphInput {
 
     private String _txStatus;
 
-    private boolean _dco;
-
-    private String _sequence;
-
     public String getPrimarySite() {
         return _primarySite;
     }
@@ -108,15 +104,4 @@ public class MphInput {
         _txStatus = txStatus;
     }
 
-    public String getCtc() {
-        return _sequence + (_dco ? "(DCO)" : "");
-    }
-
-    public void setDco(boolean dco) {
-        _dco = dco;
-    }
-
-    public void setSequence(String sequence) {
-        _sequence = sequence;
-    }
 }

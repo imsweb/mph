9f70fb2aca20579ec25a93b24a389f2603746f56
Sewbesew Bekele <bekeles@imsweb.com>
2023-01-24 12:55:49 -0500

    2023 solid tumor updates

--- src/main/java/com/imsweb/mph/mpgroups/Mp2018HeadAndNeckGroup.java
+++ src/main/java/com/imsweb/mph/mpgroups/Mp2018HeadAndNeckGroup.java
@@ -328,7 +328,7 @@ public class Mp2018HeadAndNeckGroup extends MphGroup {
     public boolean isApplicable(String primarySite, String histology, String behavior, int year) {
         if (super.isApplicable(primarySite, histology, behavior, year))
             return true;
-        return year >= 2019 && year < 2022 && ("C754".equals(primarySite) || "C755".equals(primarySite)) && Arrays.asList("8680", "8690", "8692", "9693").contains(histology) && "3".equals(behavior);
+        return year >= 2019 && year < 2022 && ("C754".equals(primarySite) || "C755".equals(primarySite)) && Arrays.asList("8680", "8690", "8692", "8693").contains(histology) && "3".equals(behavior);
     }
 }
 
